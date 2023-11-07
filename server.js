eb60f558a8c54edc84e6ccbef7b8a96659ced8db
andrewgarrison <atg27@uw.edu>
2023-11-07 11:13:05 -0800

    added additional cache function

--- server.js
+++ server.js
@@ -24,3 +24,44 @@ async function init() {
   console.log(`running on http://localhost:${PORT}`);
 }
 init();
+
+const rGet = promisify(client.get).bind(client);
+const rSetex = promisify(client.setex).bind(client);
+
+function cache(key, ttl, slowFn) {
+  return async function (...props) {
+    const cachedResponse = await rGet(key);
+    if (cachedResponse) {
+      return cachedResponse;
+    }
+    const result = await slowFn(...props);
+    await rSetex(key, ttl, result);
+    return result;
+  };
+}
+
+async function verySlowAndExpensiveFunction() {
+  // imagine this is like a really big join on PostgreSQL
+  // or a call to an expensive API
+
+  console.log("oh no an expensive call!");
+  const p = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(new Date().toUTCString());
+    }, 5000);
+  });
+
+  return p;
+}
+
+const cachedFn = cache("expensive_call", 10, verySlowAndExpensiveFunction);
+
+// inside init, under app.get pageviews
+app.get("/get", async (req, res) => {
+  const data = await cachedFn();
+
+  res.json({
+    data,
+    status: "ok",
+  });
+});
